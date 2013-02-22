d4515ec13af05db4d8ec5bb7a70f40247c603319
Abe <abekim0607@gmail.com>
2013-02-22 07:59:07 -0500

    added user attribute to todo model, played around with css

--- architecture-examples/backbone/js/models/todo.js
+++ architecture-examples/backbone/js/models/todo.js
@@ -14,13 +14,21 @@ var app = app || {};
 		// and ensure that each todo created has `title` and `completed` keys.
 		defaults: {
 			title: '',
-			completed: false
+			completed: false,
+			user: ''
 		},
 
 		// Toggle the `completed` state of this todo item.
 		toggle: function () {
 			this.save({
 				completed: !this.get('completed')
+			});
+		}
+
+		//assign a user to the task
+		assign: function (username) {
+			this.save({
+				user: username
 			});
 		}
 
