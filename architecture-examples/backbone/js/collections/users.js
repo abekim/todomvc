9587fe728c2c6dbb069408bc29fc390146519174
Abe <abekim0607@gmail.com>
2013-02-22 07:44:22 -0500

    added collection of users

---
+++ architecture-examples/backbone/js/collections/users.js
@@ -0,0 +1,32 @@
+/*global Backbone Store*/
+var app = app || {};
+
+(function () {
+  'use strict';
+
+  // user collection
+  var UserList = Backbone.Collection.extend({
+
+    // Reference to this collection's model.
+    model: app.User,
+
+    // Save all of the user objects under the "users" namespace.
+    localStorage: new Store('users-backbone'),
+
+    // Users are sorted alphabetically
+    comparator: function (user) {
+      return user.get('name');
+    }
+  });
+
+  // Create our global collection of **Users**.
+  // initializing a temporary list of users
+
+  app.Users = new UserList();
+
+  app.Users.create({name: "Nathan"});
+  app.Users.create({name: "Abe"});
+  app.Users.create({name: "Alex"});
+  app.Users.create({name: "David"});
+
+}());
